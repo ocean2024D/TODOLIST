adaca11aef4ca9a52063df43d3c9e8682883ea7c
derya <lacin>
2024-10-25 09:46:10 +0200

    todo

---
+++ toDoList.js
@@ -0,0 +1,51 @@
+let storedTodos = localStorage.getItem("todos");
+let ToDoList;
+
+if (storedTodos) {
+  ToDoList = JSON.parse(storedTodos);
+} else {
+  ToDoList = [];
+}
+
+const ul = document.getElementsByClassName("list")[0];
+
+function loadTodos() {
+  ul.innerHTML = "";
+  createElements();
+}
+
+function addTodo() {
+  const AddInput = document.querySelector("#input").value;
+  if (AddInput.trim() !== "") {
+    ToDoList.push(AddInput);
+    localStorage.setItem("todos", JSON.stringify(ToDoList));
+    createElements();
+ AddInput= "";
+  }
+}
+
+function createElements() {
+  ul.innerHTML = "";
+  for (let i = 0; i < ToDoList.length; i++) {
+    let li = document.createElement("li");
+    li.innerText = ToDoList[i];
+    ul.appendChild(li);
+
+    let button = document.createElement("button");
+    button.innerText = "X";
+    button.addEventListener("click", function () {
+      deleteTodo(i);
+    });
+    li.appendChild(button);
+  }
+}
+function deleteTodo(index) {
+  if (index >= 0 && index < ToDoList.length) {
+    ToDoList.splice(index, 1);
+    localStorage.setItem("todos", JSON.stringify(ToDoList));
+
+    createElements();
+  }
+}
+
+loadTodos();
